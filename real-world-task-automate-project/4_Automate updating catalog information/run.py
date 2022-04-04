5d7d169cba4fdc6732696b249afcbac12f973d0a
aimclee <dhy04057@likelion.org>
2022-04-04 09:59:39 +0900

    dict-to-json-post run.py added

--- real-world-task-automate-project/4_Automate updating catalog information/run.py
+++ real-world-task-automate-project/4_Automate updating catalog information/run.py
@@ -11,13 +11,23 @@ Avocado
 Avocado contains large amount of oleic acid, a type of monounsaturated fat that can replace saturated fat in the diet, which is very effective in reducing cholesterol levels. Avocado is al$
 '''
 
-for txt_file in os.listdir('./supplier-data/descriptions/'):
+def data(url, description_dir):
   json_dict={}
-  with open(txt_file) as txt_file:
-    txt_file.readlines()
-    json_dict['name'] = txt_file[0].strip('\n')
-    json_dict['weight'] = int(txt_file[1].strip('\n').split('lbs'))
-    json_dict['description'] = txt_file[2]
-    json_dict['image_name'] = txt_file.strip('.txt')+'.jpeg'
-  response = requests.port(url, json=json_dict)
+  for txt_file in os.listdir(description_dir):
+    json_dict.clear()
+    file_name = os.path.join(description_dir, txt_file)
+    with open(file_name) as file_name:
+      line = file_name.readlines()
+      json_dict['name'] = line[0].strip('\n')
+      json_dict['weight'] = int(line[1].strip('\n').split('lbs'))
+      json_dict['description'] = line[2].strip('\n').replace(u'\xa0',u'')
+      json_dict['image_name'] = line.split('.txt')+'.jpeg'
+    response = requests.post(url, json=json_dict)
+  return 0
 
+
+if __name__ == "__main__":
+  url = 'http://localhost/fruits/'
+  user = os.getenv('USER')
+  description_dir = '/home/{}/supplier-data/descriptions/'.format(user)
+  data(url, description_dir)
